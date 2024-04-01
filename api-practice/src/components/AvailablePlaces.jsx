94fefbe4f6c945c3a366758fcae667d44784bc77
yeeun <84173008+jye104@users.noreply.github.com>
2024-04-01 17:55:44 +0900

    로딩시 State(상태)에 따라 로딩 메시지 띄우기

--- api-practice/src/components/AvailablePlaces.jsx
+++ api-practice/src/components/AvailablePlaces.jsx
@@ -2,14 +2,17 @@ import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   // Todo: Fetch available places from backend API
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
+    setIsFetching(true);
     async function fetchPlaces() {
       const response = await fetch('http://localhost:3000/places');
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
@@ -18,6 +21,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
