e0a3a52aeaef5c6632898f7ce71551b879e802c0
yeeun <84173008+jye104@users.noreply.github.com>
2024-04-01 16:55:07 +0900

    useEffect로 HTTP요청(GET) 전송하기

--- api-practice/src/components/AvailablePlaces.jsx
+++ api-practice/src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  // Todo: Fetch available places from backend API
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  fetch('http://localhost:3000/places')
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places);
+    });
+  useEffect(() => {}, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
