d27b9ea081958ca11faf124a0029286b6938c889
yeeun <84173008+jye104@users.noreply.github.com>
2024-04-15 19:54:15 +0900

    EventDetail 페이지로 데이터 불러오기, delete 버튼 동작

--- react-query-practice/src/util/http.js
+++ react-query-practice/src/util/http.js
@@ -3,6 +3,7 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient();
 
 export async function fetchEvents({ signal, searchTerm }) {
+  console.log(searchTerm);
   let url = 'http://localhost:3000/events';
 
   if (searchTerm) {
@@ -59,4 +60,36 @@ export async function fetchSelectableImages({ signal }) {
   const { images } = await response.json();
 
   return images;
+}
+
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
 }
