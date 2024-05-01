f3a8e230712819bd6a58437632ae76536c38f3bb
yeeun <84173008+jye104@users.noreply.github.com>
2024-05-01 18:11:08 +0900

    유니온 타입 지정하기

--- typescript/basics.ts
+++ typescript/basics.ts
@@ -44,3 +44,10 @@ let course = 'React - The Complete Guide';
 
 // course = 12341; 명시적인 타입을 지정하지 않아도 에러가 뜸
 // 타입 추론 방식으로 코드를 작성하는 것이 권장되는 방식 -> 불필요하게 타입을 지정하지 않아도 되기 때문
+
+// 하지만, 하나의 변수 안에 문자열과 숫자를 함께 저장해야한다면? (타입추론을 하는 경우가 아닐 경우임)
+// -> 유니톤 타입을 사용하자. (: 타입을 정의할 때 한 개 이상의 타입을 사용할 수 있다)
+let course2: string | number = 'React - The Complete Guide';
+course2 = 1234;
+
+let userName2: string | string[] = 'June'; // 문자열과 문자열 배열 2가지를 설정할 수 있음
