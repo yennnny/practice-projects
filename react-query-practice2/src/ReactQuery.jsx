3db0b422633756c312be59334ca2d1b4e9563724
yeeun <84173008+jye104@users.noreply.github.com>
2024-05-06 12:47:59 +0900

    add: fetch를 사용하여 데이터 불러오기

--- react-query-practice2/src/ReactQuery.jsx
+++ react-query-practice2/src/ReactQuery.jsx
@@ -1,5 +1,31 @@
+import { useQuery } from 'react-query';
+
 const ReactQuery = () => {
-  return <></>;
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['fetchUser'],
+    queryFn: async () => {
+      // try {
+      //   const res = await
+      //   return await res.json();
+      // } catch (err) {
+      //   console.error('Error', err);
+      // }
+      return await fetch('https://jsonplaceholder.typicode.com/users').then(
+        (res) => res.json()
+      );
+    },
+  });
+
+  if (isLoading) return <>Loading...</>;
+  if (isError) return <>{error.message}</>;
+  return (
+    <>
+      <h2 className="text-4xl">ReactQuery</h2>
+      <ul className="list-disc p-4">
+        {data && data?.map((user) => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
+  );
 };
 
 export default ReactQuery;
