96e896a6fb78fd00984d3b276aa930ac2a5f0b05
yeeun <84173008+jye104@users.noreply.github.com>
2024-05-13 11:43:44 +0900

    feat: useQueryClient와 initialData 옵션 활용하기

--- react-query-practice2/src/hooks/useUserId.jsx
+++ react-query-practice2/src/hooks/useUserId.jsx
@@ -1,12 +1,25 @@
 import axios from 'axios';
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
 export const fetchUserDetails = (userId) => {
   return axios.get('https://jsonplaceholder.typicode.com/users/' + userId);
 };
 
 const useUserId = (userId) => {
-  return useQuery(['user-id', userId], () => fetchUserDetails(userId));
+  const queryClient = useQueryClient();
+  return (
+    useQuery(['user-id', userId], () => fetchUserDetails(userId)),
+    {
+      initialData: () => {
+        const user = queryClient
+          .getQueryData('user-name')
+          ?.data.find((u) => u.id === userId);
+
+        if (user) return { data: user };
+        else return undefined;
+      },
+    }
+  );
 };
 
 export default useUserId;
