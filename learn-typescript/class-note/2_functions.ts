3c922b45c89446044be68854aa264a05b46cd538
yeeun <84173008+jye104@users.noreply.github.com>
2024-05-16 17:38:17 +0900

    add: 함수 타입(옵셔널 파라미터)

--- learn-typescript/class-note/2_functions.ts
+++ learn-typescript/class-note/2_functions.ts
@@ -14,3 +14,11 @@ function sumAdd(a: number, b: number): number {
 }
 
 sum(10, 20, 30, 40); // 함수의 파라미터를 제한하는 특성
+sumAdd(10);
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string, c?: string) {
+  // 물음표는 쓸지 안쓸지 모호할 때
+}
+log('hello world');
+log('hello ts', 'abc');
