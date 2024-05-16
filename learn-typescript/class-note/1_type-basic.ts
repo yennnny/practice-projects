f9636730070ab6fbd9c5ecd820b7899fc91a520a
yeeun <84173008+jye104@users.noreply.github.com>
2024-05-16 16:39:20 +0900

    add: 기본 타입(문자열, 숫자, 배열, 튜플, 객체, 진위값)

---
+++ learn-typescript/class-note/1_type-basic.ts
@@ -0,0 +1,27 @@
+// TS 문자
+let str: string = 'hello';
+
+// TS 숫자
+let num: number = 10;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number] = ['pangyo', 100];
+
+// TS 객체
+let obj: object = {};
+let person: object = {
+  name: 'yen',
+  age: 30,
+};
+let person2: { name: string; age: number } = {
+  name: 'yen',
+  age: 30,
+};
+
+// TS 진위값
+let show: boolean = true;
