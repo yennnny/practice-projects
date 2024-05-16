52aee765d8b413fa7bde77489ed463ec36ae47cb
yeeun <84173008+jye104@users.noreply.github.com>
2024-05-16 17:30:08 +0900

    add: 함수 타입(파라미터, 반환값)

---
+++ learn-typescript/class-note/2_functions.ts
@@ -0,0 +1,14 @@
+// 함수의 파라미터에 타입을 정의하는 방식
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+// 함수의 반환값에 타입을 정의하는 방식
+function add(): number {
+  return 10;
+}
+
+// 함수의 타입을 정의하는 방식
+function sumAdd(a: number, b: number): number {
+  return a + b;
+}
