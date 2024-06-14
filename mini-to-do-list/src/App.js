35f11c85c15ee75e2ab78d1cce9be454602711a9
yeeun <84173008+jye104@users.noreply.github.com>
2024-06-15 08:55:01 +0900

    add: List 컴포넌트 생성

--- mini-to-do-list/src/App.js
+++ mini-to-do-list/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Layout from './Layout';
 import Header from './components/Header/Header';
+import List from './components/List/List';
 
 function App() {
   return (
     <Layout>
       <Header />
+      <List />
     </Layout>
   );
 }
---
+++ mini-to-do-list/src/components/List/List.jsx
@@ -0,0 +1,7 @@
+import styles from './List.module.css';
+
+const List = () => {
+  return <div className={styles.list}>todolist</div>;
+};
+
+export default List;
