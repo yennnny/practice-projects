e56d4130510bd44193b65e6c6072b0c6e9258eac
yeeun <84173008+jye104@users.noreply.github.com>
2024-06-17 15:48:22 +0900

    add: List에 Item 컴포넌트 넣기

--- mini-to-do-list/src/components/List.jsx
+++ mini-to-do-list/src/components/List.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Item from './Item';
 
 const ListBlock = styled.div`
   flex: 1;
@@ -8,7 +9,14 @@ const ListBlock = styled.div`
 `;
 
 const List = () => {
-  return <ListBlock>TodoList</ListBlock>;
+  return (
+    <ListBlock>
+      <Item txt="프로젝트 생성하기" done={true} />
+      <Item txt="컴포넌트 스타일링하기" done={true} />
+      <Item txt="Context 만들기" done={false} />
+      <Item txt="기능 구현하기" done={false} />
+    </ListBlock>
+  );
 };
 
 export default List;
