32f3b7eda6f2c9203ee1b5e35d4c45d4abbb7b87
yeeun <84173008+jye104@users.noreply.github.com>
2024-06-18 09:49:57 +0900

    add: reducer, dispatch를 사용하여 AddItem 기능 추가하기

--- mini-to-do-list/src/components/AddItem.jsx
+++ mini-to-do-list/src/components/AddItem.jsx
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 import { MdAdd } from 'react-icons/md';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useTodoDispatch, useTodoNextId } from './Context';
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -74,14 +75,39 @@ const Input = styled.input`
 
 const AddItem = () => {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('');
+
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
+
   const onToggle = () => setOpen(!open);
+  const onChange = (e) => setValue(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: 'CREATE',
+      todo: {
+        id: nextId.current,
+        txt: value,
+        done: false,
+      },
+    });
+    setValue('');
+    setOpen(false);
+    nextId.current += 1;
+  };
 
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
-            <Input autoFocus placeholder="할 일을 입력 후, Enter를 누르세요" />
+          <InsertForm onSubmit={onSubmit}>
+            <Input
+              autoFocus
+              placeholder="할 일을 입력 후, Enter를 누르세요"
+              onChange={onChange}
+              value={value}
+            />
           </InsertForm>
         </InsertFormPositioner>
       )}
@@ -92,4 +118,4 @@ const AddItem = () => {
   );
 };
 
-export default AddItem;
+export default React.memo(AddItem);
