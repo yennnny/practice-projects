cef2ee209d4a0c335951d087ef4e6fbcf843413e
yeeun <84173008+jye104@users.noreply.github.com>
2024-06-21 14:34:32 +0900

    add: 읽기 기능 구현

--- next-app/src/app/read/[id]/page.js
+++ next-app/src/app/read/[id]/page.js
@@ -1,8 +1,12 @@
-export default function Read(props) {
+// 서버 컴포넌트
+export default async function Read(props) {
+  const res = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await res.json();
   return (
     <>
-      <h2>Read</h2>
+      <h2>{topic.title}</h2>
       paramters: {props.params.id}
+      <p>{topic.body}</p>
     </>
   );
 }
