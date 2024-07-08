b8e4e51b7921ececd0def5bc370602b3a47bd7de
yeeun <84173008+jye104@users.noreply.github.com>
2024-07-08 20:40:00 +0900

    add: 페이지 및 레이아웃 작업, 커스텀 컴포넌트

---
+++ next-app2/app/about/page.js
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About us</h1>
+    </main>
+  );
+};
+
+export default AboutPage;
---
+++ next-app2/components/header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+    </>
+  );
+};
+
+export default Header;
