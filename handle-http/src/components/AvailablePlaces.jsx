bf20c6a3300225c8f58873823c1fd3bcca8c28fd
yeeun <84173008+jye104@users.noreply.github.com>
2024-08-05 17:12:08 +0900

    update: async, await으로 업데이트

--- handle-http/src/components/AvailablePlaces.jsx
+++ handle-http/src/components/AvailablePlaces.jsx
@@ -6,11 +6,15 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   // 컴포넌트가 실행될 때마다 fetch로 데이터를 새롭게 받아오지 않도록 useEffect 사용 => 컴포넌트가 한 번 생성된 후 한 번만 실행됨
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then((res) => res.json())
-      .then((resData) => setAvailablePlaces(resData.places));
-    // fetch는 Promise를 반환
-    // then 메소드에 함수를 전달하여 정의하면 Promise가 해결되고 response를 받고나서 한 번 실행됨(자동으로 response 객체를 받음)
+    // useEffect에 asnyc를 바로 사용하는건 불가능하지만, useEffect 안에 함수를 만들어 넣는건 가능.
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      // fetch는 Promise를 반환
+      // then 메소드에 함수를 전달하여 정의하면 Promise가 해결되고 response를 받고나서 한 번 실행됨(자동으로 response 객체를 받음)
+    }
+    fetchPlaces();
   }, []);
 
   return (
